332c287c437ac3ff1e1768e58c8032c355bc47cb
Jerald-CS <jeraldhuang26@crosspointacademy.org>
2025-10-27 09:04:50 -0700

    Pokemon v2

--- Pokemon.java
+++ Pokemon.java
@@ -1,20 +1,43 @@
 public class Pokemon {
     private String name;
     private int level;
+    private String move1;
+    private String move2;
+    private String move3;
+    private String move4;
 
-    // Constructor with parameters
-    public Pokemon(String n, int l) {
+    //make pokemon
+    public Pokemon(String n, int l, String m1, String m2, String m3, String m4) {
         name = n;
         level = l;
+        move1 = m1;
+        move2 = m2;
+        move3 = m3;
+        move4 = m4;
+    }
+
+    // Get methods 
+    public String getName(){ 
+        return name; 
     }
     
-
-    // Getter methods
-    public String getName() {
-        return name;
+    public int getLevel(){ 
+        return level; 
     }
-
-    public int getLevel() {
-        return level;
+    
+    public String getMove1(){ 
+        return move1;
+    }
+    
+    public String getMove2(){ 
+        return move2; 
+    }
+    
+    public String getMove3(){
+        return move3; 
+    }
+    
+    public String getMove4(){
+        return move4;
     }
 }
--- PokemonMaker.java
+++ PokemonMaker.java
@@ -1,58 +1,56 @@
 public class PokemonMaker {
     public static void main(String[] args) {
-        Pokemon p1 = new Pokemon("Dragonite", 95); //Dragon and flying type pokemon he goated 
-        System.out.println(p1.getName() + " (L." + " " + p1.getLevel() + ")");
+        Pokemon p1 = new Pokemon("Dragonite", 95, "Air Slash", "Dragon Claw", "Outrage", "Draco Meteor"); //Dragon and flying type pokemon  
+        System.out.println(p1.getName() + " (L. " + p1.getLevel() + ")");
         System.out.println("---------------------");
-        System.out.println("Air Slash");
-        System.out.println("Dragon Claw");
-        System.out.println("Outrage");
-        System.out.println("Draco Meteor");
-        System.out.println("");
+        System.out.println("1) " + p1.getMove1());
+        System.out.println("2) " + p1.getMove2());
+        System.out.println("3) " + p1.getMove3());
+        System.out.println("4) " + p1.getMove4());
+        System.out.println();
 
-        Pokemon p2 = new Pokemon("Snorlax", 96); //tanky he normal type and does decent damage
-        System.out.println(p2.getName() + " (L." + " "  + p2.getLevel() + ")");
+        Pokemon p2 = new Pokemon("Snorlax", 96, "Earthquake", "Hyper Beam", "Body Slam", "Lick"); //tanky he normal type and does decent damage
+        System.out.println(p2.getName() + " (L. " + p2.getLevel() + ")");
         System.out.println("---------------------");
-        System.out.println("Earthquake");
-        System.out.println("Hyper Beam");
-        System.out.println("Body Slam");
-        System.out.println("Lick");
-        System.out.println("");
+        System.out.println("1) " + p2.getMove1());
+        System.out.println("2) " + p2.getMove2());
+        System.out.println("3) " + p2.getMove3());
+        System.out.println("4) " + p2.getMove4());
+        System.out.println();
 
-
-        Pokemon p3 = new Pokemon("Gyarados", 97); //Water and flying 
-        System.out.println(p3.getName() + " (L." + " " + p3.getLevel() + ")");
+        Pokemon p3 = new Pokemon("Gyarados", 97, "Hydro Pump", "Waterfall", "Aqua Tail", "Outrage"); //Water and flying 
+        System.out.println(p3.getName() + " (L. " + p3.getLevel() + ")");
         System.out.println("---------------------");
-        System.out.println("Hydro Pump");
-        System.out.println("Waterfall");
-        System.out.println("Aqua tail");
-        System.out.println("Outrage");
-        System.out.println("");
+        System.out.println("1) " + p3.getMove1());
+        System.out.println("2) " + p3.getMove2());
+        System.out.println("3) " + p3.getMove3());
+        System.out.println("4) " + p3.getMove4());
+        System.out.println();
 
-        Pokemon p4 = new Pokemon("Arcanine", 98); //fast attack speed and good fire 
-        System.out.println(p4.getName() + " (L." + " " + p4.getLevel() + ")");
+        Pokemon p4 = new Pokemon("Arcanine", 98, "Fire Fang", "Wild Charge", "Flamethrower", "Snarl"); //fast attack speed and good fire 
+        System.out.println(p4.getName() + " (L. " + p4.getLevel() + ")");
         System.out.println("---------------------");
-        System.out.println("Fire Fang");
-        System.out.println("Wild Charge");
-        System.out.println("Flamethrower");
-        System.out.println("Snarl");
-        System.out.println("");
+        System.out.println("1) " + p4.getMove1());
+        System.out.println("2) " + p4.getMove2());
+        System.out.println("3) " + p4.getMove3());
+        System.out.println("4) " + p4.getMove4());
+        System.out.println();
 
-        Pokemon p5 = new Pokemon("Machamp", 99); //He the second best fighting in gen 1 behind mewtwo 
-        System.out.println(p5.getName() + " (L." + " " + p5.getLevel() + ")");
+        Pokemon p5 = new Pokemon("Machamp", 99, "Dynamic Punch", "Earthquake", "Stone Edge", "Bulk Up"); //He the second best fighting in gen 1 behind mewtwo 
+        System.out.println(p5.getName() + " (L. " + p5.getLevel() + ")");
         System.out.println("---------------------");
-        System.out.println("Dynamic Punch");
-        System.out.println("Earthquake");
-        System.out.println("Stone Edge");
-        System.out.println("Bulk Up");
-        System.out.println("");
+        System.out.println("1) " + p5.getMove1());
+        System.out.println("2) " + p5.getMove2());
+        System.out.println("3) " + p5.getMove3());
+        System.out.println("4) " + p5.getMove4());
+        System.out.println();
 
-        Pokemon p6 = new Pokemon("Alakazam", 100); //special and psychic type attacker 
-        System.out.println(p6.getName() + " (L." + " " + p6.getLevel() + ")");
+        Pokemon p6 = new Pokemon("Alakazam", 100, "Psychic", "Focus Blast", "Shadow Ball", "Recover"); //special and psychic type attacker 
+        System.out.println(p6.getName() + " (L. " + p6.getLevel() + ")");
         System.out.println("---------------------");
-        System.out.println("Psychic");
-        System.out.println("Focus Blast");
-        System.out.println("Shadow Ball");
-        System.out.println("Recover");
-
+        System.out.println("1) " + p6.getMove1());
+        System.out.println("2) " + p6.getMove2());
+        System.out.println("3) " + p6.getMove3());
+        System.out.println("4) " + p6.getMove4());
     }
 }
